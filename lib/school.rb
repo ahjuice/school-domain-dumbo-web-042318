036ad616710634697b80e90e1228a5f5850d1eee
ahjuice <ahjuice@users.noreply.github.com>
2018-04-25 17:26:24 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    unless self.roster.key?(grade)
+      self.roster[grade] = []
+    end
+    self.roster[grade] << name
+  end
+
+  def grade(grade)
+    self.roster[grade]
+  end
+
+  def sort
+    sorted_roster = self.roster.inject({}) { | roster, (grade, students) |
+      roster[grade] = students.sort; roster
+    }
+  end
+end
